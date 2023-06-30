2a3fb57828e13dad0c8cce4f5dd4c1ae85bf08f7
Dipak <88022763+im-DG@users.noreply.github.com>
2023-06-30 10:49:32 +0545

    Update ViewRemainder.jsx

--- src/Components/ViewRemainder.jsx
+++ src/Components/ViewRemainder.jsx
@@ -1,38 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 const ViewRemainder = () => {
     const navigator = useNavigate()
-    const [formData, setFormData] = useState(null);
+    const [formDataArray, setFormDataArray] = useState([]);
 
     useEffect(() => {
-        // Retrieve the form data from localStorage
-        const storedData = localStorage.getItem('formData');
+        // Retrieve the form data array from localStorage
+        const storedFormDataArray = localStorage.getItem('formDataArray');
 
-        if (storedData) {
-            // Parse the stored data as JSON
-            const parsedData = JSON.parse(storedData);
+        if (storedFormDataArray) {
+            // Parse the stored form data array as JSON
+            const parsedFormDataArray = JSON.parse(storedFormDataArray);
 
-            // Set the form data in the component state
-            setFormData(parsedData);
+            // Set the form data array in the component state
+            setFormDataArray(parsedFormDataArray);
         }
     }, []);
 
     return (
         <div>
-            {formData && (
+            {formDataArray.length > 0 ? (
                 <div>
-                    <h2>View Remainders</h2>
-                    <p>Date: {formData.date}</p>
-                    <p>Subject: {formData.subject}</p>
-                    <p>Description: {formData.description}</p>
-                    <p>Email: {formData.email}</p>
-                    <p>Contact Number: {formData.contact}</p>
-                    <p>SMS Number: {formData.smsNo}</p>
-                    <p>Recur: {formData.recurr}</p>
+                    <h2>Form Submissions</h2>
+                    {formDataArray.map((formData, index) => (
+                        <div key={index}>
+                            <h3>Form Submission {index + 1}</h3>
+                            <p>Date: {formData.date}</p>
+                            <p>Subject: {formData.subject}</p>
+                            <p>Description: {formData.description}</p>
+                            <p>Email: {formData.email}</p>
+                            <p>Contact Number: {formData.contact}</p>
+                            <p>SMS Number: {formData.smsNo}</p>
+                            <p>Recur: {formData.recurr}</p>
+                            <hr />
+                        </div>
+                    ))}
                 </div>
+            ) : (
+                <p>No form submissions yet.</p>
             )}
+
             <button type="button" onClick={() => navigator("/HomePage")}> Back</button>
-        </div>
+        </div >
     );
 };
 
