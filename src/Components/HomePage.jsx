518cfd3c0ec6672627ed1a1d397ec812d3d5a9a3
Dipak <88022763+im-DG@users.noreply.github.com>
2023-06-30 10:23:25 +0545

    Update HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -5,11 +5,14 @@ function HomePage() {
         localStorage.clear();
         window.location.reload();
     }
-    const date = Date();
+    const date = new Date();
+    const day = date.toLocaleString('default', { weekday: "long" });
+    const time = date.getDate();
+    const month = date.toLocaleString('default', { month: 'long' });
     return (
         <>
             <h1>Welcome to Reminder Application </h1>
-            <h2>Today is </h2>
+            <h2>Today is {day} {time} of {month} </h2>
             <NavLink to='/SetRemainder' > <h2>SetRemainder</h2></NavLink> <br />
             <NavLink to='/ModifyRemainder' ><h2>ModifyReminder</h2></NavLink><br />
             <NavLink to='/DeleteRemainder' ><h2>DeleteRemainder</h2></NavLink><br />
